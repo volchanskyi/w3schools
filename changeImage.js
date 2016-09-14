3708c47fde772b96cf0adb4afce71e0a247624a7
volchanskyi <volchanskij@gmail.com>
2016-09-13 20:08:59 -0700

    commit1

---
+++ changeImage.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Ivan on 9/9/2016.
+ */
+function myFunction() {
+    document.getElementById('demo').innerHTML = "My first JavaScript";
+}
---
+++ windowAlert.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Ivan on 9/9/2016.
+ */
+function alrt() {
+    window.alert(5 + 6);
+}
