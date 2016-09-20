3e193f7b037127bc60825b064c16d14755fb41f2
volchanskyi <volchanskij@gmail.com>
2016-09-20 11:58:00 -0700

    js StringMethods

--- JS/jsStringMethods.js
+++ JS/jsStringMethods.js
@@ -22,3 +22,54 @@ function mySearch() {
     var pos = str.search("locate");
     document.getElementById("demo").innerHTML = pos;
 }
+
+
+function mySlice() {
+    var str = "Apple, Banana, Kiwi";
+    var res = str.slice(7,13);
+    document.getElementById("demo").innerHTML = res;
+
+}
+
+
+function myReverseSlice() {
+    var str = "Apple, Banana, Kiwi";
+    var res = str.slice(-12,-6);
+    document.getElementById("demo").innerHTML = res;
+
+}
+
+function mySlice2() {
+    var str = "Apple, Banana, Kiwi";
+    var res = str.slice(6);
+    document.getElementById("demo").innerHTML = res;
+
+}
+
+
+function mySubstr() {
+    var str = "Apple, Banana, Kiwi";
+    var res = str.substr(6, 6);
+    document.getElementById("demo").innerHTML = res;
+
+}
+
+
+function myReplace() {
+    var str = document.getElementById("demo2").innerHTML;
+    var txt = str.replace("Microsoft","W3Schools");
+    document.getElementById("demo2").innerHTML = txt;
+
+}
+
+function myGlobalReplace() {
+    var str = document.getElementById("demo3").innerHTML;
+    var txt = str.replace(/Microsoft/g, "W3Schools");
+    document.getElementById("demo3").innerHTML = txt;
+}
+
+
+function myStingToUpperCase() {
+    var text = document.getElementById("demo4").innerHTML;
+    document.getElementById("demo4").innerHTML = text.toUpperCase();
+}
