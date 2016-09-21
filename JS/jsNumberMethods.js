9c23a90cbc5ff394221cad0d1618d9a1216aaf39
volchanskyi <volchanskij@gmail.com>
2016-09-21 14:53:01 -0700

    js Numbers

---
+++ JS/jsNumberMethods.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 9/21/2016.
+ */
+//jsNumbersMethods
+
+var x = 123;
+console.log(x.toString());
+console.log((123).toString());
+console.log((100 + 23).toString());
+
