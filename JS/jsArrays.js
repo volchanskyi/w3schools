a10962417b0bc2df7c6bd72d119e3da20201f822
volchanskyi <volchanskij@gmail.com>
2016-09-28 15:41:31 -0700

    js Arrays

--- JS/jsArrays.js
+++ JS/jsArrays.js
@@ -2,6 +2,7 @@
  * Created by Administrator on 9/22/2016.
  */
 var fruits, text, fLen, i;
+var points = [40, 100, 1, 5, 25, 10];
 function myArray() {
     
 
@@ -39,4 +40,20 @@ function myWrongArray() {
         text += fruits[i] + "<br>";
     }
     document.getElementById("demo6").innerHTML = text;
+}
+
+
+function myRigthArray() {
+    document.getElementById("demo6").innerHTML = points[0] + " " + "<b>" + "The new keyword only complicates the code. It can also produce some unexpected results. Use [] instead." + "</b>";
+    
+}
+
+function myArrayTypeOfCheck() {
+    document.getElementById("demo6").innerHTML = Array.isArray(points) + " " + "This is how you can recognize if it`s array or an object";
+
+}
+
+function myArrayTypeOfCheck2() {
+    fruits = ["banana", "Orange", "Apple", "Mango"];
+    document.getElementById("demo6").innerHTML = fruits instanceof Array;
 }
