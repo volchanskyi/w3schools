7c8e955db03e90ac7a48d558b573a6a1cd0c4deb
volchanskyi <volchanskij@gmail.com>
2016-10-11 17:24:55 -0700

    jsRegExp

---
+++ JS/jsTypeConversion.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 10/11/2016.
+ */
+
+function myConvert() {
+    var x =123;
+    document.getElementById("demo10").innerHTML = 
+        String(x) + "<br>" +
+            String(321) + "<br>" + 
+            String(100 + 23);
+}
+
+
