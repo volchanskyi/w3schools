be52900b696ad2640e876c5946ddd70032d4334b
volchanskyi <volchanskij@gmail.com>
2016-10-16 18:53:06 -0700

    jsErrors

--- JS/jsRegExp.js
+++ JS/jsRegExp.js
@@ -4,6 +4,38 @@
 
 
 function myRegExpSearch() {
-    var str = /Ivan/i;
-    var n = str.search()
+    var str = document.getElementById("demo11").value;
+    console.log(str);
+    var n = str.search("a");
+    console.log(n);
+    document.getElementById("regExp").innerHTML = n;
+}
+
+function myReplaceRegExp() {
+    var str = document.getElementById("demo11").value;
+    var txt = str.replace("Microsoft", "Ivan`s company");
+    document.getElementById("regExp").innerHTML = txt;
+}
+
+function myReplaceRegExp2() {
+    var str = document.getElementById("demo11").value;
+    var txt = str.replace(/Microsoft/g, "Ivan`s company");
+    document.getElementById("regExp").innerHTML = txt;
+}
+
+function myRegExpPatterns() {
+    var str = document.getElementById("demo11").value;
+    var pattern = str.match(/[0-5]/g);
+    var pattern2 = str.match(/[a-c]/g);
+    document.getElementById("regExp").innerHTML = pattern + "<br>" + pattern2;
+}
+
+function myCheckRegExp() {
+    txt = document.getElementById("demo11").value;
+    document.getElementById("regExp").innerHTML = /Ivan/.test(txt);
+}
+
+function myExecMethodRegExp() {
+    str = document.getElementById("demo11").value;
+    document.getElementById("regExp").innerHTML = /something is wrong/.exec(str);
 }
