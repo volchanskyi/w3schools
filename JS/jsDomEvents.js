35e8ac87055f255e8f28dc50dd51fc2841f0fcb1
volchanskyi <volchanskij@gmail.com>
2016-11-11 14:13:01 -0800

    jsDomeEvents

--- JS/jsDomEvents.js
+++ JS/jsDomEvents.js
@@ -4,3 +4,13 @@
 function displayDateTime() {
     document.getElementById("demo19").innerHTML = Date();
 }
+
+function checkCoockies() {
+        var text = "";
+        if (navigator.cockieEnabled === true) {
+            text = "Coockies are enabled";
+        } else {
+            text = "Coockies are not enabled";
+        }
+        document.getElementById("demo19").innerHTML = text;
+    }
