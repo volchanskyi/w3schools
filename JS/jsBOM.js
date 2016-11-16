41618093266213336dce81033e52eb20b6dc0076
volchanskyi <volchanskij@gmail.com>
2016-11-15 22:21:46 -0800

    jsBOM

--- JS/jsBOM.js
+++ JS/jsBOM.js
@@ -15,3 +15,28 @@ function myBOM() {
     var x = document.getElementById("demo25");
     x.innerHTML = "Browser inner window width: " + w + ", height: " + h + ".";
 }
+
+function myBOMScreenWidthAndHeight() {
+    document.getElementById("demo25").innerHTML = 
+        "Screen width is " + screen.width;
+    document.getElementById("demo26").innerHTML =
+        "Screen height is " + screen.height;
+}
+
+
+//Window Screen Available Width and Height
+
+function myBOMScrAvailWidthAndHeight() {
+    document.getElementById("demo25").innerHTML =
+        "Available screen width is " + screen.availWidth;
+
+    document.getElementById("demo26").innerHTML =
+        "Available screen height is " + screen.availHeight;
+}
+
+// The screen.pixelDepth property returns the pixel depth of the screen.
+
+function myBOMScrPixDepth() {
+    document.getElementById("demo26").innerHTML =
+        "Screen Pixel Depth: " + screen.pixelDepth;
+}
