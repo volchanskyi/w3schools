cc28ae6cfb394ff9ea6e7f7b44d00bc5abbc7cce
volchanskyi <volchanskij@gmail.com>
2016-11-26 21:40:09 -0800

    jsAjax

--- JS/jsAjax.js
+++ JS/jsAjax.js
@@ -93,6 +93,35 @@ function myXmlResponse() {
 }
 
 
+//Fetch info from XML using AJAX
+
+function myLoadXmlFetch() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            myFetchFunc(this);
+        }
+    };
+    xhttp.open("GET", "cd_catalog.xml", true);
+    xhttp.send();
+}
+
+
+function myFetchFunc(xml) {
+    var i;
+    var xmlDoc = xml.responseXML;
+    var table = "<tr><th>Artist</th><th>Title</th></tr>";
+    var x = xmlDoc.getElementsByTagName("CD");
+    for (i = 0; i < x.length; i++) {
+        table += "<tr><td>" +
+                x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
+                "</td><td>" +
+                x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
+                "</td></tr>";
+    }
+    document.getElementById("demo33").innerHTML = table;
+}
+
 
 
 
